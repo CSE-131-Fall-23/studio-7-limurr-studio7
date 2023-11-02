925e710d596664200d7fad6977fae1d1bdb4c454
richard <richard@localhost>
2023-11-02 10:49:44 -0500

    hellow

--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -9,24 +9,40 @@ public class HockeyPlayer {
 	private int games;
 	private int points;
 	
-	HockeyPlayer(String name, int number, String shoot, int goals, int assists, int games){
+	HockeyPlayer(String name, int number, String shoot){
 		this.name = name;
 		this.number = number;
 		this.shoot = shoot;
-		this.assists = assists;
-		this.goals = goals;
-		this.games = games;
+		this.assists = 0;
+		this.goals = 0;
+		this.games = 0;
 		points = assists + goals;
 	}
 	
-	public void react() {
+	public void gameComplete(int goals, int assists) {
 		System.out.println("we won!!!");
+		this.goals += goals;
+		this.assists += assists;
+		games ++;
+		
 	}
 	
 	public String toString() {
 		return "Complex{name = " + name + ", number = " + number 
 				+ ", shoot = " + shoot + ", goals = " + goals + ", assists = " + assists + 
 				", points = " + points + ", games = " + games +"}";
+	}
+	
+	public void setGoals(int goals) {
+		this.goals = goals;
+	}
+	
+	public void setGames(int games) {
+		this.games = games;
+	}
+	
+	public void setAssists(int assists) {
+		this.assists = assists;
 	}
 	
 }
